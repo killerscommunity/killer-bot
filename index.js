b6591d6787981ca84b1af93515f0081a2cd9efb9
mircogamer2011 <91784731+mircogamer2011@users.noreply.github.com>
2022-02-20 11:59:10 +0100

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client(
     { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
 )
 
-client.login("OTQ0Njg4NDExNDA1NjAyODU3.YhFPpA.cV4X6ZvMS40i4ERX2g4s4S6KJNI")
+client.login(proces.env.token)
 
 client.on("ready", () => {
     console.log("Bot ONLINE")
